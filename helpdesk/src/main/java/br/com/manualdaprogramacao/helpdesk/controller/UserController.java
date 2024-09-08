c5d7115b1667ea9cba6d7d7dc513b94ae81d1b38
Antonioafj <antonioafj.edu@gmail.com>
2024-09-08 18:15:08 -0400

    criado camada entity repository repesctivas classes arquivo sql yaml no resources
    Signed-off-by: Antonioafj <antonioafj.edu@gmail.com>

---
+++ helpdesk/src/main/java/br/com/manualdaprogramacao/helpdesk/controller/UserController.java
@@ -0,0 +1,17 @@
+package br.com.manualdaprogramacao.helpdesk.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = "/users")
+public class UserController {
+
+        @PostMapping
+        public ResponseEntity create(){
+        System.out.println("Usuario Criado");
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ helpdesk/src/main/java/br/com/manualdaprogramacao/helpdesk/entity/UserEntity.java
@@ -0,0 +1,42 @@
+package br.com.manualdaprogramacao.helpdesk.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "users")
+public class UserEntity {
+
+    @Id
+    @Column(name = "id")
+    private UUID id;
+
+    private String username;
+
+    private String password;
+
+    private String name;
+
+    private String email;
+
+    private boolean active;
+
+    @Column(name = "created_by")
+    private UUID createdBy;
+
+    @Column(name = "created_at")
+    private Date createAt;
+
+    @Column(name = "updated_by")
+    private UUID updateBy;
+
+    @Column(name = "updated_at")
+    private Date updateAt;
+}
