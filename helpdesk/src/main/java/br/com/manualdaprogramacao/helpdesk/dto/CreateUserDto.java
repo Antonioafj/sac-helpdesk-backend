b964f50e04c6d625a87dd7b49ae1a3b24dc29316
Antonioafj <antonioafj.edu@gmail.com>
2024-09-09 14:12:03 -0400

    criado camada service dto conexão entre camadas
    Signed-off-by: Antonioafj <antonioafj.edu@gmail.com>

---
+++ helpdesk/src/main/java/br/com/manualdaprogramacao/helpdesk/dto/CreateUserDto.java
@@ -0,0 +1,9 @@
+package br.com.manualdaprogramacao.helpdesk.dto;
+
+public record CreateUserDto(
+        String username,
+        String password,
+        String name,
+        String email
+) {
+}
