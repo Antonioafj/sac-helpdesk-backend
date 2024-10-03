5eaebb2ddbadbc1162f7ffae122e1568d08c3b0a
Antonioafj <antonioafj.edu@gmail.com>
2024-10-02 21:10:52 -0400

    instalads dependencia jwt criado pacote security classe em configuration hierach filterchain
    Signed-off-by: Antonioafj <antonioafj.edu@gmail.com>

--- helpdesk/src/main/java/br/com/manualdaprogramacao/helpdesk/configuration/SecurityConfiguration.java
+++ helpdesk/src/main/java/br/com/manualdaprogramacao/helpdesk/configuration/SecurityConfiguration.java
@@ -1,21 +1,32 @@
 package br.com.manualdaprogramacao.helpdesk.configuration;
 
 
+import br.com.manualdaprogramacao.helpdesk.security.JwtTokenFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
+import org.springframework.security.access.hierarchicalroles.RoleHierarchyImpl;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfiguration {
 
-        private UserDatailsServiceImpl userDatailsService;
+        private final UserDatailsServiceImpl userDatailsService;
+
+        private final JwtTokenFilter jwtTokenFilter;
 
 
         @Bean
@@ -37,8 +48,35 @@ public class SecurityConfiguration {
         }
 
         @Bean
-        public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        public AuthenticationManager authenticationManager(
+                AuthenticationConfiguration authenticationConfiguration) throws Exception {
             return authenticationConfiguration.getAuthenticationManager();
+        }
+
+        @Bean
+        public RoleHierarchy roleHierarchy() {
+            RoleHierarchyImpl hierarchy = new RoleHierarchyImpl();
+            hierarchy.setHierarchy("ADMIN > SUPPORT_ATENDENT > CUSTUMER");
+            return hierarchy;
+        }
+
+        @Bean
+        public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+            http
+                    .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                    .requestMatchers("/swagger-ui.html").permitAll()
+                    .requestMatchers("swagger-ui**/").permitAll()
+                    .requestMatchers("api-docs/**").permitAll()
+                    .requestMatchers("/auth/token").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/users").permitAll()
+                    .anyRequest().authenticated()
+            )
+                    .formLogin(withDefaults -> withDefaults.disable())
+                    .logout(withDefaults -> withDefaults.disable())
+                    .cors(withDefaults -> withDefaults.disable())
+                    .csrf(withDefaults -> withDefaults.disable());
+            http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+            return http.build();
         }
 }
 
---
+++ helpdesk/src/main/java/br/com/manualdaprogramacao/helpdesk/security/JwtTokenFilter.java
@@ -0,0 +1,4 @@
+package br.com.manualdaprogramacao.helpdesk.security;
+
+public class JwtTokenFilter {
+}
